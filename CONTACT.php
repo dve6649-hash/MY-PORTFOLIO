71d6560843618ad5dda0d3d55555892b95ccce72
dve6649-hash <dve6649@gmail.com>
2026-03-27 16:34:43 +0500

    Add files via upload

---
+++ CONTACT.php
@@ -0,0 +1,82 @@
+<html>
+<head>
+<title>CONTACT</title>
+<style>
+body{
+    background-color: white;
+    font-family: Arial, Helvetica, sans-serif;
+    color: black;
+}
+header{
+    width: 100%;
+    background-color: white;
+}
+header .Nav-Bar{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 30px;
+    max-width: 1200px;
+    margin-top: 0;
+    margin-bottom: 0;
+    margin-left: 100px;
+    margin-right: 100px;
+    padding: 15px;
+}
+header .Nav-Bar #Logo{
+    width: 30px;
+    height: 30px;
+}
+header .Nav-Bar .Nav-Links{
+    display: flex;
+    gap: 100px;
+}
+header .Nav-Bar .Nav-Links a{
+    font-weight: bolder;
+    font-size: 16px;
+    text-decoration: none;
+    color: black;    
+}
+.Nav-Links :hover{
+    border-bottom: 2px solid black;
+}
+header #Dark{
+    background-color: white;
+    border-color: black;
+    border-radius: 50%;
+}
+
+
+
+</style>
+</head>
+<body>
+
+<header>
+        <nav class="Nav-Bar">
+            <img src="logo.webp" id="Logo">
+            <div class="Nav-Links">
+                <a href="http://localhost/SAMPLEPAGE/MYPORTFOLIO.php">Home</a>
+                <a href="http://localhost/SAMPLEPAGE/PICTURE.php">Picture</a>
+                <a href="">Contact</a>
+            </div>
+            <button id="Dark"><a href="http://localhost/SAMPLEPAGE/LOGINPAGE.php">L</a></button>
+        </nav>
+    </header>
+
+<section>
+
+<h1>Name: Allan Dave E Ebale</h1>
+<h1>Contact<h1>
+<ol>
+	<li>Phone no.</li>
+	<li>Gmail</li>
+	<li>Facebook</li>
+	<li>Tiktok</li>
+	<li>Instagram</li>
+</ol>
+
+</section>
+
+</body>
+</html>
